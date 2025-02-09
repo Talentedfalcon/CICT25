7e16f21d3de8859899946e8858972e663aaef1cb
Harith Yerragolam <yharith16@gmail.com>
2025-02-09 15:07:16 +0530

    update faculty `facebook` links correspondig to Insitute links and corrected the titles in co_patrons and patrons data

--- src/app/data/co_patrons.js
+++ src/app/data/co_patrons.js
@@ -5,17 +5,17 @@ export const Co_Patrons = {
         title: "Prof. Sivaselvan B",
         description: "IIITDM Kancheepuram, Head of the Department, Computer Science and Engineering",
         socialLinks: {
-          facebook: "https://facebook.com/mukul.sutaone",
+          facebook: "https://www.iiitdm.ac.in/people/faculty/sivaselvanb@iiitdm.ac.in",
           twitter: "https://twitter.com/mukul_sutaone",
           instagram: "https://instagram.com/mukul.sutaone",
         },
       },
       {
         image: "/assets/images/co_patrons/p2.png",
-        title: "Dr. Chitti Babu B, Head of the Department",
-        description: "IIITDM Kancheepuram, Electronics and Communication Engineering ",
+        title: "Dr. Chitti Babu B",
+        description: "IIITDM Kancheepuram, Head of the Department, Electronics and Communication Engineering ",
         socialLinks: {
-          facebook: "https://facebook.com/bhartendu.singh",
+          facebook: "https://www.iiitdm.ac.in/people/faculty/bcbabu@iiitdm.ac.in",
           twitter: "https://twitter.com/bhartendu_singh",
           instagram: "https://instagram.com/bhartendu.singh",
         },
@@ -25,7 +25,7 @@ export const Co_Patrons = {
         title: "Prof. Raja B,h",
         description: "IIITDM Kancheepuram, Head of the Department, Mechanical Engineering ",
         socialLinks: {
-          facebook: "https://facebook.com/sn.singh",
+          facebook: "https://www.iiitdm.ac.in/people/faculty/rajab@iiitdm.ac.in",
           twitter: "https://twitter.com/sn_singh",
           instagram: "https://instagram.com/sn.singh",
         },
@@ -35,7 +35,7 @@ export const Co_Patrons = {
         title: "Dr. Sudhir Varadrajan,",
         description: "IIITDM Kancheepuram, Head, School of Interdisciplinary Design and Innovation",
         socialLinks: {
-          facebook: "https://facebook.com/mv.karthikeyan",
+          facebook: "https://www.iiitdm.ac.in/people/faculty/sudhir@iiitdm.ac.in",
           twitter: "https://twitter.com/mv_karthikeyan",
           instagram: "https://instagram.com/mv.karthikeyan",
         },
@@ -45,7 +45,7 @@ export const Co_Patrons = {
         title: "Prof. Naveen Kumar",
         description: "IIITDM Kancheepuram, Head of the Department, Sciences and Humanities",
         socialLinks: {
-          facebook: "https://facebook.com/dvln.somayajulu",
+          facebook: "https://www.iiitdm.ac.in/people/faculty/nkumar@iiitdm.ac.in",
           twitter: "https://twitter.com/dvln_somayajulu",
           instagram: "https://instagram.com/dvln.somayajulu",
         },
--- src/app/data/patrons.js
+++ src/app/data/patrons.js
@@ -2,20 +2,20 @@ export const Patrons = {
   "Patrons": [
     {
       image: "/assets/images/patrons/p1.png",
-      title: "Prof. M. V. Kartikeyan, Director, IIITDM Kancheepuram   ",
+      title: "Prof. M. V. Kartikeyan",
       description: "Director, IIITDM Kancheepuram",
       socialLinks: {
-        facebook: "https://facebook.com/mukul.sutaone",
-        twitter: "https://twitter.com/mukul_sutaone",
-        instagram: "https://instagram.com/mukul.sutaone",
+        facebook: "https://www.iiitdm.ac.in/people/faculty/kartik@iiitdm.ac.in",
+        twitter: "https://twitter.com/mv_karthikeyan",
+        instagram: "https://instagram.com/mv.karthikeyan",
       },
     },
     {
       image: "/assets/images/patrons/p2.png",
-      title: "Prof. B. S. Moorthy                ",
+      title: "Prof. B. S. Murty",
       description: " Director, IIITDM Kurnool",
       socialLinks: {
-        facebook: "https://facebook.com/bhartendu.singh",
+        facebook: "https://mme.iitm.ac.in/faculty-inner.php?id=20&fc=1",
         twitter: "https://twitter.com/bhartendu_singh",
         instagram: "https://instagram.com/bhartendu.singh",
       },
@@ -25,7 +25,7 @@ export const Patrons = {
       title: "Prof. Sri Niwas Singh",
       description: "ABV-IIITM, Gwalior              ",
       socialLinks: {
-        facebook: "https://facebook.com/sn.singh",
+        facebook: "https://www.iiitm.ac.in/index.php/en/component/splms/teacher/prof-sri-niwas",
         twitter: "https://twitter.com/sn_singh",
         instagram: "https://instagram.com/sn.singh",
       },
@@ -35,7 +35,7 @@ export const Patrons = {
       title: "Prof. Bhartendu K Singh",
       description: "Director, IIITDM Jabalpur     ",
       socialLinks: {
-        facebook: "https://facebook.com/mv.karthikeyan",
+        facebook: "http://faculty.iiitdmj.ac.in/faculty/Bhartendu-K-Singh",
         twitter: "https://twitter.com/mv_karthikeyan",
         instagram: "https://instagram.com/mv.karthikeyan",
       },
@@ -45,7 +45,7 @@ export const Patrons = {
       title: "Prof. Mukul Sharad Sutaone",
       description: "Director, IIIT Allahabad  ",
       socialLinks: {
-        facebook: "https://facebook.com/dvln.somayajulu",
+        facebook: "https://iiita.irins.org/profile/147084",
         twitter: "https://twitter.com/dvln_somayajulu",
         instagram: "https://instagram.com/dvln.somayajulu",
       },
