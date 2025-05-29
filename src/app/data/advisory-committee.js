0611e4b8b2379e585ab9eb2749dc7a4237a7c95d
kokilnit <106372850+kokilnit@users.noreply.github.com>
2025-05-29 16:36:26 +0530

    Update advisory-committee.js

--- src/app/data/advisory-committee.js
+++ src/app/data/advisory-committee.js
@@ -144,7 +144,6 @@ export const InternationalAdvisoryCommittee = {
         linkedin: "https://it.linkedin.com/in/ggruosso",
       },
     },
-
      },
     {
       title: "Dr. Archana Kamal",
@@ -155,6 +154,5 @@ export const InternationalAdvisoryCommittee = {
         linkedin: "https://www.linkedin.com/in/archanakamal/",
       },
     },
-    
   ],
 };
